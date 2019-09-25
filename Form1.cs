6502179e03b2d3c26a0817ff73a82ddfe5af098e
Cam Wood <CamW5326@gmail.com>
2019-09-25 01:48:52 -0500

    webServerv1

---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CamWebServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "Start";
+            button2.Text = "Stop";
+            button2.Enabled = false;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button1.Enabled = false;
+
+            ThreadPool.QueueUserWorkItem(BackgroundFunc, this);
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+        private void BackgroundFunc(object state)
+        {
+            try
+            {
+                Thread.Sleep(5000);
+                SetForegroundData("");
+            }
+            catch (Exception Ex)
+            {
+                SetForegroundData(Ex.Message);
+            }
+        }
+        //Calls itself recursivley until it finds parent
+        public void SetForegroundData(string value)
+        {
+            if (InvokeRequired) //if background thread
+            {
+                this.BeginInvoke(new Action<string>(SetForegroundData), new object[] { value });
+                return;
+            }
+            button1.Text = value;
+        }
+    }
+}
